fd60c101d4571aae12117f8a3512d9642cbab73a
overstac <105587254+overstac@users.noreply.github.com>
2022-07-06 17:32:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from pathlib import Path
 
-root_dir = Path("files")
-file_paths= root_dir.iterdir()
+root_dir= Path("files")
+file_paths = root_dir.glob("**/*")
 
 for path in file_paths:
-  new_filename= "new-" + path.stem + path.suffix
-  new_filepath = path.with_name(new_filename)
-  print(new_filepath)
-  path.rename(new_filepath)
+  if path.is_file():
+    parent_folder= path.parts[-2] 
+    new_filename= parent_folder + "-" + path.name
+    print(new_filename)
+
+    new_filepath= path.with_name(new_filename)
+    path.rename(new_filepath)
